d2450c709c37fbf5f67f251e4f8494bfa3192940
kristinavenevtseva <63412659+kristinavenevtseva@users.noreply.github.com>
2020-05-07 12:51:21 +0300

    added all

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int base = 2_000_000_000;
+        int transit = 500_000_000;
+        int total = base + transit;
+        System.out.println(total);
+    }
+}
